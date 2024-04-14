fcbcce5bbfaffff9cc66bf4494e64caccd07c0db
Rafael Santos <rafaelcs_94@hotmail.com>
2024-04-14 16:40:25 -0300

    fix: middleware now only works for api routes

--- next-app/middleware.ts
+++ next-app/middleware.ts
@@ -1,11 +1,10 @@
 import type { NextRequest } from 'next/server'
 
 export function middleware(req: NextRequest) {
-  if (req.headers.get('authorization') !== process.env.AUTH_TOKEN) {
+  if (
+    req.nextUrl.pathname.startsWith('/api') &&
+    req.headers.get('authorization') !== process.env.AUTH_TOKEN
+  ) {
     return Response.json({ message: 'Unauthorized' }, { status: 401 })
   }
-}
-
-export const config = {
-  matcher: ['/((?!_next/static|_next/image|.*\\.png$).*)']
 }
