9e35a5f2d52545face24fa98d0b7cdb9cded4df6
Rafael Santos <rafaelcs_94@hotmail.com>
2024-04-14 15:12:19 -0300

    feat: create api/jobs/submit POST method

---
+++ next-app/app/api/jobs/submit/route.ts
@@ -0,0 +1,33 @@
+import { z } from 'zod'
+
+const schema = z.object({
+  name: z.string(),
+  age: z.number(),
+  phone: z.string(),
+  state: z.string(),
+  city: z.string()
+})
+
+export const POST = async (req: Request) => {
+  try {
+    const data = schema.parse(await req.json())
+
+    return Response.json(
+      {
+        message: `Thank you for your application, ${data.name}`
+      },
+      { status: 201 }
+    )
+  } catch (error: any) {
+    console.log(error.message)
+
+    if (error instanceof z.ZodError) {
+      return Response.json(
+        { path: error.errors[0].path[0], message: error.errors[0].message },
+        { status: 400 }
+      )
+    }
+
+    return Response.json({ message: 'Failed to submit job application' }, { status: 500 })
+  }
+}
