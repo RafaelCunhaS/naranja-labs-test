299d4ef231adb544b40d6bb528d5eefe1be26e21
Rafael Santos <rafaelcs_94@hotmail.com>
2024-04-14 22:23:41 -0300

    refactor: create Footer component

---
+++ next-app/components/Footer.tsx
@@ -0,0 +1,14 @@
+function Footer() {
+  return (
+    <div className="px-40 bg-white pt-16 w-full flex gap-20 text-4xl text-gray-1">
+      <p className="playfair-display pl-[246.5px]">SimSong</p>
+      <p className="font-medium">Harps</p>
+      <p className="vidaloka">DiskVan</p>
+      <p className="montserrat font-bold">Ikon</p>
+      <p className="nunito font-bold">Strons</p>
+      <p className="inter font-bold pr-[246.5px]">Cocoon</p>
+    </div>
+  )
+}
+
+export default Footer
